6c9c5b9ad0ecb7b29fd47ccd3af65680c7a1ddbe
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-16 10:19:47 -0500

    show pokemons

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Pokemon } from 'src/app/interface/pokemon.interface';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
+  public pokemons: Pokemon[] = [];
 
+  constructor(public pokemonService: PokemonService) {
+    this.pokemonService.getPokemons().subscribe((res) => (this.pokemons = res));
+  }
 }
