a66cb152ab70da7011b48ecbbbe815aa20ed1d7b
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-16 10:25:27 -0500

    use card component in home component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Pokemon } from 'src/app/interface/pokemon.interface';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  styleUrls: ['./card.component.css'],
 })
 export class CardComponent {
-
+  @Input() pokemon!: Pokemon;
 }
