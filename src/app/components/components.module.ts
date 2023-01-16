5f4c183809e8fabfa94bcb7a83e1bd9fb8fd6c8f
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-16 16:00:04 -0500

    ultimos cambios

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
 import { CardComponent } from './card/card.component';
 import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [SearchComponent, CardComponent],
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   exports: [SearchComponent, CardComponent],
 })
 export class ComponentsModule {}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { DetailComponent } from './detail/detail.component';
 import { ComponentsModule } from '../components/components.module';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [HomeComponent, DetailComponent],
-  imports: [CommonModule, ComponentsModule],
+  imports: [CommonModule, ComponentsModule, RouterLink],
   exports: [HomeComponent, DetailComponent],
 })
 export class PagesModule {}
