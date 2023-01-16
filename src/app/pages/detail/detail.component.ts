f59e3d01110cdf807cdf755df11b48bd8a7718dc
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-16 11:14:39 -0500

    avance

--- src/app/pages/detail/detail.component.ts
+++ src/app/pages/detail/detail.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Pokemon } from 'src/app/interface/pokemon.interface';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
 })
-export class DetailComponent {
+export class DetailComponent implements OnInit {
+  pokemon!: Pokemon;
 
+  constructor(
+    public route: ActivatedRoute,
+    private pokemonService: PokemonService
+  ) {}
+
+  ngOnInit(): void {
+    let name = this.route.snapshot.paramMap.get('name')!;
+    this.pokemonService.getPokemonData(name).subscribe((res) => {
+      this.pokemon = res;
+    });
+  }
 }
