0d3c11cb4bd286506bae2327e23b5c1c74e58f4d
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-16 10:55:51 -0500

    import routerLink

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search/search.component';
 import { CardComponent } from './card/card.component';
+import { RouterLink } from '@angular/router';
 
 @NgModule({
   declarations: [SearchComponent, CardComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   exports: [SearchComponent, CardComponent],
 })
 export class ComponentsModule {}
