879d35ca3aaceec09ca6240a269cf54bd5f656b5
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-16 10:19:24 -0500

    create function for  getPokemons

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,23 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map, mergeMap, toArray } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  BASE_URL = 'https://pokeapi.co/api/v2';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  //Recupera la lista de pokemons con sus datos
+  getPokemons() {
+    return this.http
+      .get<any>(this.BASE_URL + '/pokemon?offset=0&limit=150')
+      .pipe(
+        mergeMap((res) => res.results),
+        mergeMap((pokemon: any) => this.http.get<any>(pokemon.url)),
+        toArray()
+      );
+  }
 }
